93c93f87aa3f99907c70ef2d0f75ea13ebe85c18
Marc-AntoineJobin <2356735@csfoy.ca>
2025-04-09 09:22:48 -0400

    ajout du create film sans middleware

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -3,13 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Resources\FilmResource;
 use App\Models\Film;
+use App\Http\Resources\FilmResource;
+use App\Repository\Eloquent\FilmRepository;
+use App\Repository\FilmRepositoryInterface; // hm-hmm
+use App\Repository\Eloquent\BaseRepository;
 
 
 class FilmController extends Controller
 {   
+    private FilmRepositoryInterface $filmRepository;
 
+    public function __construct(FilmRepositoryInterface $filmRepository)
+    {
+        $this->filmRepository = $filmRepository;
+    }
+    
+    public function create(Request $request)
+    {
+        try
+        {
+            $film = $this->filmRepository->create($request->all());
+            return (new FilmResource($film))->response()->setStatusCode(CREATED);
+        }
+        
+        catch(Exception $ex)
+        {
+            abort(SERVER_ERROR, 'Server error');
+        }       
+    }
     
 }
 
