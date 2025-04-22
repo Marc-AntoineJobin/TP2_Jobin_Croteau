b3e5c76aa637b0c19a31596476a9908957a29f42
Marc-AntoineJobin <2356735@csfoy.ca>
2025-04-21 21:43:20 -0400

    trying to debug deployment

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::middleware('throttle:5,1')->group( function(){
+    Route::post('/signup', 'App\Http\Controllers\AuthController@register');
+    Route::post('/signin', 'App\Http\Controllers\AuthController@login');
+    Route::post('/signout', 'App\Http\Controllers\AuthController@logout')->middleware('auth:sanctum');
+});
+Route::middleware('auth:sanctum')->group( function(){
+    Route::middleware([AdminMiddleware::class])->group( function(){
+        Route::post('/films', 'App\Http\Controllers\FilmController@create');
+        Route::put('/films/{id}', 'App\Http\Controllers\FilmController@update');
+        Route::delete('/films/{id}', 'App\Http\Controllers\FilmController@delete');
+    });
+    Route::post('/critics', 'App\Http\Controllers\CriticController@create')->middleware([CriticMiddleware::class]);
+    
+    Route::middleware([OwnUserMiddleware::class])->group( function(){
+    Route::get('/users/{id}', 'App\Http\Controllers\UserController@show');
+    Route::put('/users/{id}', 'App\Http\Controllers\UserController@updatePassword');
+    });
+});
